6cebb742551c5b5abb68d3fc5368c6e7aa0bca37
Juan Fernando <jfdelarosa@outlook.com>
2020-05-14 00:04:28 -0500

    :lipstick: estilos base para el contenedor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { html, define } from "./helpers.js";
 import "./components/MyInput.js";
 import "./components/MyButton.js";
+import "./components/MyFigure.js";
 
 export async function onSubmit(host, event) {
   try {
@@ -39,17 +40,80 @@ export const MyApp = {
   isValid: true,
   value: "",
   render: ({ loading, isValid, value }) => html`
+    <style>
+      ::selection {
+        background: #d2e2f9;
+        color: #3f84e5;
+      }
+      .container {
+        background: white;
+        display: flex;
+        align-items: center;
+        border-radius: 1rem;
+        box-shadow: 0 0 2px rgba(9, 31, 63, 0.2), 0 0 4px rgba(9, 31, 63, 0.2),
+          0 0 8px rgba(9, 31, 63, 0.2), 0 0 16px rgba(9, 31, 63, 0.2),
+          0 0 32px rgba(9, 31, 63, 0.2);
+        padding: 4rem 5rem;
+        justify-content: space-around;
+      }
+      .form-container > * {
+        margin: 2rem 0;
+      }
+      form {
+      }
+      h1 {
+        color: #2e81f5;
+        font-size: 3rem;
+        font-weight: 700;
+        margin: 0;
+      }
+      h2 {
+        color: #7989a0;
+        font-weight: 600;
+        font-size: 1.25rem;
+        line-height: 1.5;
+      }
+
+      .form-container,
+      .figure {
+        flex: 1;
+      }
+
+      .figure {
+        display: none;
+      }
+      @media (min-width: 768px) {
+        .figure {
+          display: block;
+        }
+        .form-container {
+          padding-right: 3rem;
+        }
+      }
+    </style>
     <div class="container">
-      ${isValid ? "" : errorMessage()}
-      <my-input
-        disabled="${loading}"
-        onmy-input=${onChange}
-        onmy-submit="${onSubmit}"
-      ></my-input>
-      <my-button
-        disabled="${loading || !isValid || value === ""}"
-        onmy-click="${onSubmit}"
-      ></my-button>
+      <div class="form-container">
+        <h1>¡Suscríbete!</h1>
+        <h2>
+          Obten todas las novedades de nuestro sitio web en tu bandeja de
+          entrada.
+        </h2>
+        <div class="form">
+          ${isValid ? "" : errorMessage()}
+          <my-input
+            disabled="${loading}"
+            onmy-input=${onChange}
+            onmy-submit="${onSubmit}"
+          ></my-input>
+          <my-button
+            disabled="${loading || !isValid || value === ""}"
+            onmy-click="${onSubmit}"
+          ></my-button>
+        </div>
+      </div>
+      <div class="figure">
+        <my-figure></my-figure>
+      </div>
     </div>
   `,
 };
